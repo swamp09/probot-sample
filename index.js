b566c0252f701c5b192a8430d84f7a7b0ce10282
swamp09 <shu.numata@esm.co.jp>
2018-12-17 21:15:02 +0900

    Run `yarn create probot-app probot-sample`

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  app.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
+}
